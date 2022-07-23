c692e2bdd18113cabf6762c1bf3165b928cebdce
Eleicul <silvaluci987@gmail.com>
2022-07-23 00:32:59 -0300

    Mentoria e curso

---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,41 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso {
+    Private String título;
+    Private String descrição;
+    Private int cargaHorária;
+
+    public Curso() {
+    }
+
+    public Private getString() {
+        return String;
+    }
+
+    public void setString(Private string) {
+        String = string;
+    }
+
+    public int getCargaHorária() {
+        return cargaHorária;
+    }
+
+    public void setCargaHorária(int cargaHorária) {
+        this.cargaHorária = cargaHorária;
+    }
+
+    @Override
+    public java.lang.String toString() {
+        return "Curso{" +
+                "String=" + String +
+                ", String=" + String +
+                ", cargaHorária=" + cargaHorária +
+                '}';
+    }
+
+    public void setdescrição(java.lang.String descrição_curso_java) {
+    }
+
+    public void setTítulo(java.lang.String curso_java) {
+    }
+}
